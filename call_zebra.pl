c7f3b8e09934c1819bf476bbe6334b1c61bb89af
Oscar D. Arnaiz <arnaiz@users.noreply.github.com>
2019-04-24 01:33:54 +0200

    must call zebra

--- call_zebra.pl
+++ call_zebra.pl
@@ -5,4 +5,4 @@ use CGI;
 my $title="\"Title of the report\"";
 my $log_file="./resource.log";
 
-print `/usr/local/sbin/show_log.pl $log_file $title`;
+print `/usr/local/sbin/zebra.pl $log_file $title`;
